deabcef36c86ca9fc723b9e2fa6829525e9475b4
Md. Rasel Hossain <raselhossain5437@gmail.com>
2021-12-28 12:07:59 +0600

    180118

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace AgroProductProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description pages.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact pages.";
 
             return View();
         }
