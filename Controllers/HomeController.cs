a4382dba39b5552d5f9f8988b1ef73f58296eae1
sohan2133 <sohan.just.cse19@gmail.com>
2021-12-28 10:57:34 +0600

    180138

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace AgroProductProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page here.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page here.";
 
             return View();
         }
