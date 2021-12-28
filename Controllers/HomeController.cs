1d43fa35722592cf72e58aeea77aa1d9973ab29c
souravkunduss1@outlook.com <souravkunduss1@outlook.com>
2021-12-28 11:19:50 +0600

    180144

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace AgroProductProject.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page here.";
+            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page here.";
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
